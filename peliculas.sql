1e6d68c8f1ee0a905cfa03f40543160a96ab88df
Romina Pineda <romi.macka@gmail.com>
2020-09-27 23:12:43 -0300

    Create Tables

--- peliculas.sql
+++ peliculas.sql
@@ -1,3 +1,16 @@
 -- Crear base de datos llamada películas
 CREATE DATABASE peliculas;
 \c peliculas;
+-- Revisar los archivos peliculas.csv y reparto.csv para crear las tablas correspondientes, determinando la relación entre ambas tablas.
+CREATE TABLE peliculas (
+    id INT NOT NULL UNIQUE,
+    titulo VARCHAR(150) NOT NULL,
+    anio_estreno INT,
+    director VARCHAR(50),
+    PRIMARY KEY (id)
+);
+CREATE TABLE reparto (
+    id_peliculas INT NOT NULL,
+    nombre VARCHAR(150) NOT NULL,
+    FOREIGN KEY (id_peliculas) REFERENCES peliculas(id)
+);
