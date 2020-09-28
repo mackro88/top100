bff1e8fbcdc432e4c5e52a3aef6eb9eb9ab6afc6
Romina Pineda <romi.macka@gmail.com>
2020-09-27 23:53:19 -0300

    Top 10 directores query ready

--- peliculas.sql
+++ peliculas.sql
@@ -21,3 +21,5 @@ CREATE TABLE reparto (
 SELECT nombre, titulo, anio_estreno, director FROM reparto INNER JOIN peliculas ON reparto.id_peliculas = peliculas.id WHERE id_peliculas = (SELECT id FROM peliculas WHERE titulo = 'Titanic');
 -- Listar los titulos de las películas donde actúe Harrison Ford
 SELECT titulo AS Películas_Harrison_Ford FROM peliculas INNER JOIN reparto ON peliculas.id = reparto.id_peliculas WHERE nombre = 'Harrison Ford';
+-- Listar los 10 directores mas populares, indicando su nombre y cuántas películas aparecen en el top 100.
+SELECT director, count(director) AS peliculas FROM peliculas GROUP BY director ORDER BY count(director) DESC LIMIT 10; 
